08c6bf2cb0e8c3db84624f238f01548c6c020877
Kraldr <amaurymedina5@gmail.com>
2021-09-08 18:04:58 -0500

    Prueba master

--- app/src/main/java/com/example/primera/listadmin.kt
+++ app/src/main/java/com/example/primera/listadmin.kt
@@ -3,8 +3,8 @@ package com.example.primera
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import com.example.primera.databinding.ActivityListadminBinding
 import com.example.primera.databinding.ActivityRegistrarseBinding
 
@@ -16,6 +16,21 @@ class listadmin : AppCompatActivity() {
         setContentView(R.layout.activity_listadmin)
 
         var btnCrearTipo = findViewById<Button>(R.id.btnCrearTipo)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val list = resources.getStringArray(R.array.typeArchive)
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, list)
+        spinner.adapter = adaptador
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
 
         btnCrearTipo.setOnClickListener {
             val intent = Intent(this, regisType::class.java)
